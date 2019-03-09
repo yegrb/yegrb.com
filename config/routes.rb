8b14a7fe60d722702b49f19c9cf2dcdd6146cb57
Joshua MacSween <joshmacsween@gmail.com>
2019-03-09 09:57:07 -0700

    Updated Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
-  get '/pages' => 'pages#index'
-  get 'pages/meetup' => 'pages#meetup'
-  get 'pages/contact' => 'pages#contact'
-  get 'pages/about' => 'pages#about'
+  get '/pages' , to: 'pages#index'
+  get 'pages/meetup' , to: 'pages#meetup'
+  get 'pages/contact' , to: 'pages#contact'
+  get 'pages/about' , to: 'pages#about'
 end
