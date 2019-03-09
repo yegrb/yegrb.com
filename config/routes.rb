d0aedf48a2d5566d78618f095a38142639dc4d69
Alan Vardy <alanvardy@gmail.com>
2019-03-09 14:31:55 -0700

    Shorten routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   root 'pages#index'
-  get '/pages' , to: 'pages#index'
-  get 'pages/meetup' , to: 'pages#meetup'
-  get 'pages/contact' , to: 'pages#contact'
-  get 'pages/about' , to: 'pages#about'
+  get '/meetup' , to: 'pages#meetup'
+  get '/contact' , to: 'pages#contact'
+  get '/about' , to: 'pages#about'
 end
