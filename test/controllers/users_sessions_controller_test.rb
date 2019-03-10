90ff55b1b4849dccef78cbae0c985ec164ea8f84
Alan Vardy <alanvardy@gmail.com>
2019-03-10 14:21:55 -0600

    Additional login tests

---
+++ test/controllers/users_sessions_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersSessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+  test "should get login" do
+    get new_user_session_path
+    assert_response :success
+  end
+
+  test 'should log in user with correct password' do
+    post user_session_path, params: { user: {email: @user.email, password: 'password'}}
+    assert_redirected_to root_path
+  end
+
+  test 'shouldn\'t log in user with incorrect password' do
+    post user_session_path, params: { user: {email: @user.email, password: 'nottherightpassword'}}
+    assert_response :success
+  end
+end
