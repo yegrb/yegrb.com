ec6b05033eb57e5b4a3d8af98e4f6512abba9e0d
Alan Vardy <alanvardy@gmail.com>
2019-03-12 10:10:39 -0600

    Add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ApplicationRecord
 
          has_many :posts
 
+  ROLES = ['admin', 'user']
+
   validates :first_name, presence: true
   validates :email, presence: true
+  validates :role, presence: true, inclusion: { in: ROLES, message: "%{value} is not a valid role"}
+
+  # set role to user if blank
+  before_validation do |user|
+    user.role ||= 'user'
+  end
+
+  # Attributes:
+  # email:                  string
+  # first_name:             string
+  # role:                    string
+  # created_at, updated_at: datetime
+
+  def admin?
 end
