9d614790266d286410f60e365effae60d0dde6c8
Alan Vardy <alanvardy@gmail.com>
2019-03-14 13:29:42 -0600

    Add opportunities CRUD

---
+++ db/migrate/20190314192535_create_opportunities.rb
@@ -0,0 +1,16 @@
+class CreateOpportunities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :opportunities do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :company
+      t.string :contact
+      t.string :email
+      t.boolean :paid_position
+      t.text :content
+      t.datetime :good_until
+
+      t.timestamps
+    end
+  end
+end
