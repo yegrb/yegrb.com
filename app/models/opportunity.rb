0357711cb420eec48ea08d24e4ea9882747d0c08
Alan Vardy <alanvardy@gmail.com>
2019-03-15 10:31:55 -0600

    Add attrbute details

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,3 +1,16 @@
 class Opportunity < ApplicationRecord
   belongs_to :user
+
+  # Attributes
+  # user_id:                  integer
+  # title:                    string
+  # company:                  string
+  # contact:                  string
+  # email:                    string
+  # paid_position:            boolean
+  # content:                  text
+  # good_until:               datetime
+  # created_at, updated_at:   datetime
+
+
 end
