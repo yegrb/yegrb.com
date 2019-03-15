689a8f97b575e9e6802e75f377fe99841263c29f
Alan Vardy <alanvardy@gmail.com>
2019-03-15 09:39:11 -0600

    Fix current tests to match authorization

--- test/controllers/opportunities_controller_test.rb
+++ test/controllers/opportunities_controller_test.rb
@@ -5,44 +5,49 @@ class OpportunitiesControllerTest < ActionDispatch::IntegrationTest
     @opportunity = opportunities(:one)
   end
 
-  test "should get index" do
+  test "should get index when not logged in" do
     get opportunities_url
     assert_response :success
   end
 
-  test "should get new" do
+  test "shouldn't get new when not logged in" do
     get new_opportunity_url
-    assert_response :success
+    assert_redirected_to root_path
   end
 
   test "should create opportunity" do
-    assert_difference('Opportunity.count') do
+    assert_no_difference('Opportunity.count') do
       post opportunities_url, params: { opportunity: { company: @opportunity.company, contact: @opportunity.contact, content: @opportunity.content, email: @opportunity.email, good_until: @opportunity.good_until, paid_position: @opportunity.paid_position, title: @opportunity.title, user_id: @opportunity.user_id } }
     end
 
-    assert_redirected_to opportunity_url(Opportunity.last)
+    # assert_redirected_to opportunity_url(Opportunity.last)
+    assert_redirected_to root_path
   end
 
-  test "should show opportunity" do
+  test "should show opportunity when not logged in" do
     get opportunity_url(@opportunity)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "shouldn't get edit when not logged in" do
     get edit_opportunity_url(@opportunity)
-    assert_response :success
+    assert_redirected_to root_path
   end
 
-  test "should update opportunity" do
-    patch opportunity_url(@opportunity), params: { opportunity: { company: @opportunity.company, contact: @opportunity.contact, content: @opportunity.content, email: @opportunity.email, good_until: @opportunity.good_until, paid_position: @opportunity.paid_position, title: @opportunity.title, user_id: @opportunity.user_id } }
-    assert_redirected_to opportunity_url(@opportunity)
+  test "shouldn't update opportunity when not logged in" do
+    patch opportunity_url(@opportunity), params: { opportunity: { company: @opportunity.company, contact: @opportunity.contact, content: @opportunity.content, email: @opportunity.email, good_until: @opportunity.good_until, paid_position: @opportunity.paid_position, title: 'NEWTITLE', user_id: @opportunity.user_id } }
+    @opportunity.reload
+    assert_not_equal 'NEWTITLE', @opportunity.title
+    # assert_redirected_to opportunity_url(@opportunity)
+    # assert_redirected_to root_path
   end
 
-  test "should destroy opportunity" do
-    assert_difference('Opportunity.count', -1) do
+  test "shouldn't destroy opportunity" do
+    assert_no_difference('Opportunity.count') do
       delete opportunity_url(@opportunity)
     end
 
-    assert_redirected_to opportunities_url
+    # assert_redirected_to opportunities_url
+    assert_redirected_to root_path
   end
 end
