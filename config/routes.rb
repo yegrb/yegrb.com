f422b617d033c5827b1aca453a50d9212f8333d6
Alan Vardy <alanvardy@gmail.com>
2019-03-21 15:49:34 -0600

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # == Route Map
 #
 #                                Prefix Verb   URI Pattern                                                                              Controller#Action
+#                                 users GET    /users(.:format)                                                                         users#index
+#                                       POST   /users(.:format)                                                                         users#create
+#                              new_user GET    /users/new(.:format)                                                                     users#new
+#                             edit_user GET    /users/:id/edit(.:format)                                                                users#edit
+#                                  user GET    /users/:id(.:format)                                                                     users#show
+#                                       PATCH  /users/:id(.:format)                                                                     users#update
+#                                       PUT    /users/:id(.:format)                                                                     users#update
+#                                       DELETE /users/:id(.:format)                                                                     users#destroy
 #                         opportunities GET    /opportunities(.:format)                                                                 opportunities#index
 #                                       POST   /opportunities(.:format)                                                                 opportunities#create
 #                       new_opportunity GET    /opportunities/new(.:format)                                                             opportunities#new
@@ -23,7 +31,7 @@
 #                                       GET    /opportunities(.:format)                                                                 pages#opportunities
 #                                 slack GET    /slack(.:format)                                                                         pages#slack
 #                             resources GET    /resources(.:format)                                                                     pages#resources
-#                                       GET    /check.txt(.:format)                                                                     #<Proc:0x000055f9ebac68f0@/home/vardy/coding/yegrb.com/config/routes.rb:74>
+#                                       GET    /check.txt(.:format)                                                                     #<Proc:0x00005600ab444240@/home/vardy/coding/yegrb.com/config/routes.rb:60>
 #           rails_amazon_inbound_emails POST   /rails/action_mailbox/amazon/inbound_emails(.:format)                                    action_mailbox/ingresses/amazon/inbound_emails#create
 #         rails_mandrill_inbound_emails POST   /rails/action_mailbox/mandrill/inbound_emails(.:format)                                  action_mailbox/ingresses/mandrill/inbound_emails#create
 #         rails_postmark_inbound_emails POST   /rails/action_mailbox/postmark/inbound_emails(.:format)                                  action_mailbox/ingresses/postmark/inbound_emails#create
@@ -46,9 +54,15 @@
 #                  rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   resources :opportunities
   resources :events
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+
 
   root 'pages#index'
   get '/contact' , to: 'pages#contact'
