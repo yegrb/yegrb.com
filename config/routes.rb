44521de6025fc3b04e6ab2e09c71f350c67cc5f7
Alan Vardy <alanvardy@gmail.com>
2019-03-21 14:05:44 -0600

    Remove old controllers

--- config/routes.rb
+++ config/routes.rb
@@ -17,28 +17,13 @@
 #                                       PATCH  /events/:id(.:format)                                                                    events#update
 #                                       PUT    /events/:id(.:format)                                                                    events#update
 #                                       DELETE /events/:id(.:format)                                                                    events#destroy
-#                      new_user_session GET    /users/sign_in(.:format)                                                                 users/sessions#new
-#                          user_session POST   /users/sign_in(.:format)                                                                 users/sessions#create
-#                  destroy_user_session DELETE /users/sign_out(.:format)                                                                users/sessions#destroy
-#                     new_user_password GET    /users/password/new(.:format)                                                            devise/passwords#new
-#                    edit_user_password GET    /users/password/edit(.:format)                                                           devise/passwords#edit
-#                         user_password PATCH  /users/password(.:format)                                                                devise/passwords#update
-#                                       PUT    /users/password(.:format)                                                                devise/passwords#update
-#                                       POST   /users/password(.:format)                                                                devise/passwords#create
-#              cancel_user_registration GET    /users/cancel(.:format)                                                                  devise/registrations#cancel
-#                 new_user_registration GET    /users/sign_up(.:format)                                                                 devise/registrations#new
-#                edit_user_registration GET    /users/edit(.:format)                                                                    devise/registrations#edit
-#                     user_registration PATCH  /users(.:format)                                                                         devise/registrations#update
-#                                       PUT    /users(.:format)                                                                         devise/registrations#update
-#                                       DELETE /users(.:format)                                                                         devise/registrations#destroy
-#                                       POST   /users(.:format)                                                                         devise/registrations#create
 #                                  root GET    /                                                                                        pages#index
 #                               contact GET    /contact(.:format)                                                                       pages#contact
 #                                 about GET    /about(.:format)                                                                         pages#about
 #                                       GET    /opportunities(.:format)                                                                 pages#opportunities
 #                                 slack GET    /slack(.:format)                                                                         pages#slack
 #                             resources GET    /resources(.:format)                                                                     pages#resources
-#                                       GET    /check.txt(.:format)                                                                     #<Proc:0x000055696d4d4ff0@/home/vardy/coding/yegrb.com/config/routes.rb:13>
+#                                       GET    /check.txt(.:format)                                                                     #<Proc:0x000055f9ebac68f0@/home/vardy/coding/yegrb.com/config/routes.rb:74>
 #           rails_amazon_inbound_emails POST   /rails/action_mailbox/amazon/inbound_emails(.:format)                                    action_mailbox/ingresses/amazon/inbound_emails#create
 #         rails_mandrill_inbound_emails POST   /rails/action_mailbox/mandrill/inbound_emails(.:format)                                  action_mailbox/ingresses/mandrill/inbound_emails#create
 #         rails_postmark_inbound_emails POST   /rails/action_mailbox/postmark/inbound_emails(.:format)                                  action_mailbox/ingresses/postmark/inbound_emails#create
