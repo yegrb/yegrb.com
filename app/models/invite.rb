4671cb9890e68e9ab24da625278320eddece3672
Alan Vardy <alanvardy@gmail.com>
2019-03-22 16:36:40 -0600

    Validations etc

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -13,4 +13,21 @@
 #
 
 class Invite < ApplicationRecord
+  belongs_to :user
+  before_create :set_attributes
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :role, presence: true, inclusion: { in: User::ROLES }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: User::VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  def set_attributes
+    self.code = Digest::SHA1.hexdigest(email)
+    self.expiry = Time.now + 1.week
+  end
+
+  def expired?
+    expiry > Time.now
+  end
 end
