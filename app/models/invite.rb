a5559726ccf353399e2afe32998f04bf34d8b481
Alan Vardy <alanvardy@gmail.com>
2019-03-22 14:31:00 -0600

    Annotate

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: invites
+#
+#  id         :bigint(8)        not null, primary key
+#  code       :string
+#  email      :string
+#  expiry     :datetime
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Invite < ApplicationRecord
 end
--- test/models/invite_test.rb
+++ test/models/invite_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: invites
+#
+#  id         :bigint(8)        not null, primary key
+#  code       :string
+#  email      :string
+#  expiry     :datetime
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 require 'test_helper'
 
 class InviteTest < ActiveSupport::TestCase
