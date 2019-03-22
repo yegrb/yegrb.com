923384b658a874cfe2470bd916f9493ad3382d48
Alan Vardy <alanvardy@gmail.com>
2019-03-21 20:28:18 -0600

    Improve flash messages and remove json methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) || Rails.env.development?
       log_in user
+      flash[:success] = "Welcome #{user.first_name}"
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,31 +25,24 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        flash[:success] = 'User was successfully created.'
-        format.html { redirect_to @user }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = 'User was successfully created.'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Could not create user'
+      render :new
     end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        flash[:success] = 'User was successfully updated.'
-        format.html { redirect_to @user }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:success] = 'User was successfully updated.'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'Could not update user'
+      render :edit
     end
   end
 
@@ -57,21 +50,19 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      flash[:success] = 'User was successfully destroyed.'
-      format.html { redirect_to users_url}
-      format.json { head :no_content }
-    end
+    flash[:success] = 'User was successfully destroyed.'
+    redirect_to users_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :role, :password, :password_confirmation)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :role, :password, :password_confirmation)
+  end
 end
