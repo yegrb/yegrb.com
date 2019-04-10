ce37c88bc3c09a9ea08b9bc14dde5bb6790b27a6
Alan Vardy <alanvardy@gmail.com>
2019-04-09 20:35:24 -0600

    expired invite

--- test/factories.rb
+++ test/factories.rb
@@ -1,8 +1,8 @@
- FactoryBot.define do
+FactoryBot.define do
   factory :video do
-    link { "MyString" }
-    title { "MyString" }
-    speaker { "MyString" }
+    link { 'MyString' }
+    title { 'MyString' }
+    speaker { 'MyString' }
     user_id { 1 }
   end
 
@@ -10,15 +10,19 @@
     name { Faker::Name.unique.name }
     email { Faker::Internet.unique.email }
     code { Faker::Crypto.unique.md5 }
-    role { "user" }
-    expiry { Time.now + 3600 }
-    association :user, factory: :user, role: "editor"
+    role { 'user' }
+    expiry { Time.zone.now + 3600 }
+    association :user, factory: :user, role: 'editor'
+  end
+
+  factory :expired_invite, parent: :invite do
+    expiry { Time.zone.now - 10 }
   end
 
   factory :user, class: User do
     first_name { Faker::Name.unique.first_name }
     last_name  { Faker::Name.unique.last_name }
-    role  { "user" }
+    role  { 'user' }
     email { Faker::Internet.unique.email }
     password { Faker::Internet.password(10, 20) }
     password_confirmation { password }
@@ -26,11 +30,11 @@
   end
 
   factory :editor, parent: :user do
-    role  { "editor" }
+    role  { 'editor' }
   end
 
   factory :admin, parent: :user do
-    role  { "admin" }
+    role  { 'admin' }
   end
 
   factory :opportunity, class: Opportunity do
@@ -41,13 +45,13 @@
     email { Faker::Internet.email }
     paid_position { true }
     content { Faker::Restaurant.review }
-    good_until { Time.now + 3600 }
+    good_until { Time.zone.now + 90.days }
   end
 
   factory :event, class: Event do
     user
     title { Faker::Restaurant.unique.name }
-    time { Time.now + 3600 }
+    time { Time.zone.now + 3600 }
     location { Faker::Address.full_address }
     signup_link { Faker::Internet.url }
     content { Faker::Restaurant.review }
