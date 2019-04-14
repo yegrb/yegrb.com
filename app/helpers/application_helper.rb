b2c601a8d97533111d5ed0250c034f8cd65a3dcb
Alan Vardy <alanvardy@gmail.com>
2019-04-14 09:37:36 -0600

    use helper for active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include SessionsHelper
+
+  def nav_link(variable, titles)
+    if (titles.is_a?(String) && titles == variable) ||
+       (titles.is_a?(Array) && titles.include?(variable))
+      'nav-link active'
+    else
+      'nav-link'
+    end
+  end
 end
